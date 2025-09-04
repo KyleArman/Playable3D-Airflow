2b8494152992fac9fcdda1a5dc698a90e4a1ee7f
KyleArman <muhamad.haikal@graduate.utm.my>
2025-09-04 12:28:13 +0800

    dada
    adad

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "KhairulIzzul-ARAircondSimulation-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
     "Build/3046af113a742da88288652245f35eef.framework.js",
-    "Build/5658df6b42f3a972634a5c06ad28a319.data",
-    "Build/4f4d00d302af2ec77482cac86ef56222.wasm",
+    "Build/d3d09e3f154b62edde6f04bffeba3996.data",
+    "Build/729ff6f74e555c52e6f888321d9596bb.wasm",
     "TemplateData/style.css"
 
 ];
