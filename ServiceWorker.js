52774df45c1333aac182bbbaaa3a5693685a0d27
KyleArman <muhamad.haikal@graduate.utm.my>
2025-09-08 23:06:25 +0800

    Update UI
    Update UI Udara Panas n Sejuk

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "KhairulIzzul-ARAircondSimulation-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/123eb99bdae60e519345dc6a0ae72a3a.framework.js",
-    "Build/37e4bd5ded2a4aa1514a85b00b9f7f90.data",
-    "Build/c85e951cb52478acabb2cbb1c24576f0.wasm",
+    "Build/f82e37022a8cf23bab8773977413b012.framework.js",
+    "Build/14f33f31d218298f46c4098f5a19fbc7.data",
+    "Build/2d0e5b2471551f5829981a740f084dd7.wasm",
     "TemplateData/style.css"
 
 ];
